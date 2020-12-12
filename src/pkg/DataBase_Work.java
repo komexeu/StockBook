d7a03e383b852a2a3bf465b3c405c1d18f32b253
4A6K0092 <4A6K0092@stust.edu.tw>
2020-12-12 17:03:24 +0800

    #feat:Insert Data.

--- src/pkg/DataBase_Work.java
+++ src/pkg/DataBase_Work.java
@@ -1,5 +1,6 @@
 package pkg;
 
+import javax.naming.Name;
 import javax.swing.table.DefaultTableModel;
 import java.sql.*;
 import java.util.Vector;
@@ -7,30 +8,33 @@ import java.util.Vector;
 public class DataBase_Work {
     private static Connection conn;
     private static Statement stmt;
-    private static  DefaultTableModel _tableModel = new DefaultTableModel();
+    private static DefaultTableModel _tableModel;
 
-    DataBase_Work(){
+    DataBase_Work() {
         ConnectDataBase();
     }
 
-    void ConnectDataBase(){
-        try{
+    void ConnectDataBase() {
+        try {
             conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/stockbook_db?serverTimezone=UTC", "root", "password");
             stmt = conn.createStatement();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e);
         }
     }
 
-    void DoSomething(String order) {
+    void Search(String order) {
         try {
             //"SELECT * FROM stock_db"
-            ResultSet rs = stmt.executeQuery(order);
+            String complax_order = "SELECT * FROM stock_db WHERE ID =" + order + ";";
+            System.out.println(complax_order);
+            ResultSet rs = stmt.executeQuery(complax_order);
             ResultSetMetaData rsmd = rs.getMetaData();
             Vector<String> columnNames = new Vector<>();
             Vector<Vector<String>> data = new Vector<>();
 
+            _tableModel = new DefaultTableModel();
             for (int i = 0; i < rsmd.getColumnCount(); i++) {
                 columnNames.add(rsmd.getColumnName(i + 1));
                 _tableModel.addColumn(rsmd.getColumnName(i + 1));
@@ -50,7 +54,22 @@ public class DataBase_Work {
         }
     }
 
-    DefaultTableModel GetTableModel(){
+    void Add_Data(String ID, String NAME, String BUY, String SELL) {
+        try {
+            //"INSERT INTO stock_db(ID,NAME,BUY,SELL) VALUES();"
+            String complax_order = "INSERT INTO stock_db(ID,NAME,BUY,SELL) VALUES(\""
+                     + ID + "\",\"" + NAME + "\",\"" + BUY + "\",\"" + SELL + "\");";
+            System.out.println(complax_order);
+            stmt.executeUpdate(complax_order);
+
+            Search(ID);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+
+    DefaultTableModel GetTableModel() {
         return _tableModel;
     }
 }
--- src/pkg/GUI.java
+++ src/pkg/GUI.java
@@ -9,46 +9,93 @@ import java.awt.event.ActionListener;
 public class GUI {
     JFrame _frame = new JFrame("First GUI");
 
-    JPanel _text_button_panel = new JPanel();
-    JTextField _text = new JTextField("",15);
-    JButton _button = new JButton("BUTTON");
+    JPanel _search_panel = new JPanel();
+    JLabel _search = new JLabel();
+    JTextField _text = new JTextField("", 15);
+    JButton _search_button = new JButton("搜尋");
+
+    JPanel _newData_panel = new JPanel();
+    JLabel _ID_label = new JLabel();
+    JTextField _ID_text = new JTextField("", 10);
+    JLabel _NAME_label = new JLabel();
+    JTextField _NAME_text = new JTextField("", 15);
+    JLabel _BUY_label = new JLabel();
+    JTextField _BUY_text = new JTextField("", 10);
+    JLabel _SELL_label = new JLabel();
+    JTextField _SELL_text = new JTextField("", 10);
+    JButton _newData_button = new JButton("新增資料");
 
     JPanel _panel = new JPanel();
     JTable _tb = new JTable();
-    DefaultTableModel _dtm =new DefaultTableModel();
+    DefaultTableModel _dtm = new DefaultTableModel();
     JScrollPane _scrollPane;
-//----------------------
-    DataBase_Work db_work=new DataBase_Work();
+    //----------------------
+    DataBase_Work db_work = new DataBase_Work();
 
     GUI() {
-        _frame.setSize(500, 500);
+        _frame.setSize(1000, 500);
         _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        _search.setText("ID檢索 : ");
+        _search_panel.add(_search);
+        _search_panel.add(_text);
+        _search_panel.add(_search_button);
+
+        _ID_label.setText("ID : ");
+        _NAME_label.setText("名稱 : ");
+        _BUY_label.setText("買進 : ");
+        _SELL_label.setText("賣出 : ");
+        _newData_panel.add(_ID_label);
+        _newData_panel.add(_ID_text);
+        _newData_panel.add(_NAME_label);
+        _newData_panel.add(_NAME_text);
+        _newData_panel.add(_BUY_label);
+        _newData_panel.add(_BUY_text);
+        _newData_panel.add(_SELL_label);
+        _newData_panel.add(_SELL_text);
+        _newData_panel.add(_newData_button);
+
         _tb.setModel(_dtm);
-        _scrollPane =new JScrollPane(_tb);
-
-        _text_button_panel.add(_text);
-        _text_button_panel.add(_button);
-        _button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               db_work.DoSomething(_text.getText());
-               UpdateTableModel(db_work.GetTableModel());
-            }
-        });
-
-        _text_button_panel.add(_scrollPane);
+        _scrollPane = new JScrollPane(_tb);
 
         _panel.setLayout(new BoxLayout(_panel, BoxLayout.Y_AXIS));
-        _panel.add(_text_button_panel);
+        _panel.add(_search_panel);
+        _panel.add(_newData_panel);
         _panel.add(_scrollPane);
 
         _frame.getContentPane().add(_panel);
         _frame.setVisible(true);
+
+        Action();
     }
 
-    void UpdateTableModel(DefaultTableModel dtm){
-        _dtm=dtm;
+    void Action() {
+        _search_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String order_text = _text.getText();
+                if (order_text.length() != 4)
+                    return;
+                for (int i = 0; i < order_text.length(); ++i)
+                    if (!Character.isDigit(order_text.charAt(i)))
+                        return;
+
+                db_work.Search(_text.getText());
+                UpdateTableModel(db_work.GetTableModel());
+            }
+        });
+
+        _newData_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                db_work.Add_Data(_ID_text.getText(),_NAME_text.getText(),_BUY_text.getText(),_SELL_text.getText());
+                UpdateTableModel(db_work.GetTableModel());
+            }
+        });
+    }
+
+    void UpdateTableModel(DefaultTableModel dtm) {
+        _dtm = dtm;
         _tb.setModel(_dtm);
     }
 }
